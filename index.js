95ba0df5364a40715eefdd02fe0bb0d290f61f79
Alicia BELCHIOR <alicia.belchior@outlook.com>
2024-11-30 23:16:41 +0100

    modify an user

--- index.js
+++ index.js
@@ -45,6 +45,33 @@ app.post("/", (req, res) => {
 	res.status(201).json(newUser)
 })
 
+//PUT: Modifier un utilisateur en fonction de son ID
+app.put("/:id", (req, res) => {
+
+    // récupérer toutes les données qui arrivent dans le corps de la requête (body)
+	const { firstName, lastName } = req.body
+    
+    const id = parseInt(req.params.id)
+
+    // trouve son index, verifier si le userIndex est positive
+	const userIndex = users.findIndex((user) => user.id === id)
+
+    // utilisateur non trouvé
+	if (userIndex < 0)
+		return res.status(404).json({ msg: "utilisateur non trouvé" })
+
+    // s'il est trouvé, nous vérifions quelles valeurs ont été envoyées
+	if (firstName) users[userIndex].firstName = firstName
+	if (lastName) users[userIndex].lastName = lastName
+
+    res.json({
+        msg: "utilisateur mis à jour",
+        user: users[userIndex],
+      })      
+
+  })
+
+
 
 app.delete("/", (req, res) => {
         res.json({
